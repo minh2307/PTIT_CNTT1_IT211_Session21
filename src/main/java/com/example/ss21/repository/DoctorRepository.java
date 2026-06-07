916a972f3f0a5499a5691ed7fe38967071eea582
HoangCuongCat <Cathoang162006@gmail.com>
2026-06-07 22:30:19 +0700

    Initial commit: Doctor Management CRUD

---
+++ src/main/java/com/example/ss21/repository/DoctorRepository.java
@@ -0,0 +1,10 @@
+package com.example.ss21.repository;
+
+
+import com.example.ss21.entity.Doctor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+}
---
+++ src/test/java/com/example/ss21/Ss21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ss21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ss21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
