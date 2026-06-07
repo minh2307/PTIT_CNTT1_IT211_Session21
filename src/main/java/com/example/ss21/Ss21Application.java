7c91972e3c2a47ffb35cdab93947168a5b8663e7
minh2307 <minh23923@gmail.com>
2026-06-07 22:07:59 +0700

    skeleton

---
+++ src/main/java/com/example/ss21/Ss21Application.java
@@ -0,0 +1,13 @@
+package com.example.ss21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ss21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ss21Application.class, args);
+	}
+
+}
